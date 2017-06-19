0bf122c27862dc2c2f1d0ffca5ec1998aa57e306
rougehero <stannajl@miamioh.edu>
2017-06-19 11:48:43 -0400

    controller and layout work

--- app/controllers/rusty_engine/recentmessage_controller.rb
+++ app/controllers/rusty_engine/recentmessage_controller.rb
@@ -1,9 +1,17 @@
 require_dependency "rusty_engine/application_controller"
 
 module RustyEngine
-  class RecentmessageController < ApplicationController
+  class ApplicationController < ::ApplicationController
     def index
-      
+      if session[:user_id] = user.id
+        @conversation = Conversation.find(params[:conversation_last])
+        @messages = @conversation.messages
+        i = 0
+        begin
+          @messages[i]
+          i += 1
+        end until i > 2
+      end 
     end
 
   end
